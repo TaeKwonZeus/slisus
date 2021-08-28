ac7967bf30841ca8ef977995f2909f5d4fdf9d9d
baka <dyatlovleonid@gmail.com>
2021-08-28 18:43:54 +0300

    added contains functions

--- contains.go
+++ contains.go
@@ -2,7 +2,7 @@ package slisus
 
 import "reflect"
 
-// Contains checks if the specified element is contained in slice.
+// Contains checks if slice contains elem.
 func Contains(slice []interface{}, elem interface{}) bool {
 	for _, i := range slice {
 		if reflect.DeepEqual(i, elem) {
@@ -13,6 +13,7 @@ func Contains(slice []interface{}, elem interface{}) bool {
 	return false
 }
 
+// ContainsAll checks if slice contains all elems.
 func ContainsAll(slice []interface{}, elems []interface{}) bool {
 	for _, i := range elems {
 		if !Contains(slice, i) {
@@ -21,4 +22,26 @@ func ContainsAll(slice []interface{}, elems []interface{}) bool {
 	}
 
 	return true
+}
+
+// MapContainsKey checks if gomap's keys contain key.
+func MapContainsKey(gomap map[interface{}]interface{}, key interface{}) bool {
+	for i := range gomap {
+		if reflect.DeepEqual(i, key) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// MapContainsValue checks if gomap's values contain value.
+func MapContainsValue(gomap map[interface{}]interface{}, value interface{}) bool {
+	for _, i := range gomap {
+		if reflect.DeepEqual(i, value) {
+			return true
+		}
+	}
+
+	return false
 }
